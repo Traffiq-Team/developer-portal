90decc1637e20403d32609b2c9b4ab4b6183fa2e
chefong <ericong18@gmail.com>
2021-10-03 11:05:53 -0700

    Set up form fields in dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Page from '../../components/Page';
+import styles from './styles.module.css';
 
 const Dashboard = () => {
-  return <Page>Dashboard</Page>;
+  const [url, setUrl] = useState('');
+  const [targetLatency, setTargetLatency] = useState(0);
+
+  const handleSave = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Page>
+      <h1>Dashboard</h1>
+      <form className={styles.form} onSubmit={handleSave}>
+        <label>
+          URL
+          <input
+            className={styles.input}
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </label>
+        <label>
+          Target latency (in milliseconds)
+          <input
+            className={styles.input}
+            type="number"
+            value={targetLatency}
+            onChange={(e) => setTargetLatency(e.target.value)}
+          />
+        </label>
+        <button type="submit">Save</button>
+      </form>
+    </Page>
+  );
 };
 
 export default Dashboard;
