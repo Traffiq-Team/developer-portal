88542b1964dc0a47bee11ae52097f20371181989
chefong <ericong18@gmail.com>
2021-10-02 23:24:57 -0700

    Add react router dom

---
+++ src/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
