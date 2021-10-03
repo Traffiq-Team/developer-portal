75c392a7a51a95aff58bf48a92a5ccd6f1255d3e
chefong <ericong18@gmail.com>
2021-10-02 23:56:42 -0700

    Add react context

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import React, { createContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
+import reducer from './reducer';
+
+const store = createContext({});
+const { Provider } = store;
+
+const initialState = { val: 'hi' };
+
+function GlobalProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <Provider value={{ state, dispatch }}>{children}</Provider>;
+}
+
+GlobalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export { GlobalProvider };
+export default store;
