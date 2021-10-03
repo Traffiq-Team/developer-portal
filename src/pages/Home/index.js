e790905b3b5f67b6c54049382295ecbf21f6258c
chefong <ericong18@gmail.com>
2021-10-02 23:48:33 -0700

    Add form fields for home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './styles.module.css';
 import Page from '../../components/Page';
 
 const Home = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('submitting', username, password);
+  };
+
   return (
-    <Page>Home</Page>
+    <Page>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input className={styles.input} type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password
+          <input className={styles.input} type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </Page>
   );
 };
 
