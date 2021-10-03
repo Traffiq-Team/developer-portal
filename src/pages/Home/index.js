f5d894cbb80b72e2654dce5011f0de2da733a5b4
chefong <ericong18@gmail.com>
2021-10-03 10:44:52 -0700

    Use reducer to set authentication and redirect to dashboard page after logging in

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
-import styles from './styles.module.css';
+import React, { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Page from '../../components/Page';
+import AuthContext from '../../store/AuthProvider';
+import { SET_IS_AUTHENTICATED } from '../../store/AuthProvider/actions';
+import styles from './styles.module.css';
 
 const Home = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const { authState, authDispatch } = useContext(AuthContext);
+  const { isAuthenticated } = authState;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.push('/dashboard');
+    }
+  }, [isAuthenticated]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submitting', username, password);
+
+    // TODO: Call endpoint to handle authentication
+    authDispatch({ type: SET_IS_AUTHENTICATED, payload: true });
   };
 
   return (
--- src/store/AuthProvider/reducer.js
+++ src/store/AuthProvider/reducer.js
@@ -1,5 +1,9 @@
+import { SET_IS_AUTHENTICATED } from './actions';
+
 const reducer = (state, { type, payload }) => {
   switch (type) {
+    case SET_IS_AUTHENTICATED:
+      return { ...state, isAuthenticated: payload };
     default:
   }
 };
