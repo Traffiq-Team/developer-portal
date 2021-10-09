34e304bd4f36bbb29a63bf9b2d6f93a1946f95a5
Eric Ong <ericong18@gmail.com>
2021-10-09 00:00:08 -0700

    Fix edit icon click

--- src/components/icons/EditIcon.js
+++ src/components/icons/EditIcon.js
@@ -12,6 +12,7 @@ const EditIcon = ({ className, onClick }) => {
       role="img"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 576 512"
+      onClick={onClick}
     >
       <path
         fill="currentColor"
