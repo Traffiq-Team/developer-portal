2afa1a2fffd549ab9105d942a50622d0cf620d54
Eric Ong <ericong18@gmail.com>
2021-10-09 11:05:27 -0700

    Add delete functionality

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,37 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import classNames from 'classnames';
+import { Dialog } from 'evergreen-ui';
 import Page from '../../components/Page';
 import getAllAppConfigurations from '../../api/getAllAppConfigurations';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import EditIcon from '../../components/icons/EditIcon';
 import TrashIcon from '../../components/icons/TrashIcon';
+import deleteAppConfiguration from '../../api/deleteAppConfiguration';
 import styles from './styles.module.css';
 
 const Dashboard = () => {
   const [appConfigurations, setAppConfigurations] = useState([]);
+  const [focusedAppName, setFocusedAppName] = useState('');
+  const [isDeleting, setIsDeleting] = useState(false);
   const history = useHistory();
 
-  useEffect(() => {
-    const _getAllAppConfigurations = async () => {
-      try {
-        const appConfigurations = await getAllAppConfigurations();
-        setAppConfigurations(appConfigurations);
-      } catch (error) {
-        console.error('error from _getAllAppConfigurations', error);
-      }
-    };
+  const populateAppConfigurations = async () => {
+    try {
+      const appConfigurations = await getAllAppConfigurations();
+      setAppConfigurations(appConfigurations);
+    } catch (error) {
+      console.error('error from _getAllAppConfigurations', error);
+    }
+  };
 
-    _getAllAppConfigurations();
+  useEffect(() => {
+    populateAppConfigurations();
   }, []);
 
   const handleEditClick = (appName) => {
     history.push(`/app/${appName}`);
   };
 
-  const handleDeleteClick = (appName) => {
-    history.push(`/app/${appName}`);
+  const handleDeleteClick = async () => {
+    setIsDeleting(true);
+
+    try {
+      await deleteAppConfiguration(focusedAppName);
+      setFocusedAppName('');
+    } catch (error) {
+      console.error('error from handleDeleteClick', error);
+    } finally {
+      setIsDeleting(false);
+
+      // Fetch the app configurations again after making changes
+      populateAppConfigurations();
+    }
   };
 
   return (
@@ -76,7 +92,7 @@ const Dashboard = () => {
                     />
                     <TrashIcon
                       className={classNames(styles.icon, styles.trashIcon)}
-                      onClick={() => handleDeleteClick(appName)}
+                      onClick={() => setFocusedAppName(appName)}
                     />
                   </span>
                 </td>
@@ -85,6 +101,18 @@ const Dashboard = () => {
           </tbody>
         </table>
       </section>
+      <Dialog
+        isShown={!!focusedAppName}
+        intent="danger"
+        confirmLabel="Delete"
+        title="Delete app"
+        onConfirm={handleDeleteClick}
+        onCloseComplete={() => setFocusedAppName('')}
+        isConfirmLoading={isDeleting}
+        preventBodyScrolling
+      >
+        Are you sure you want to delete <strong>{focusedAppName}</strong>?
+      </Dialog>
     </Page>
   );
 };
